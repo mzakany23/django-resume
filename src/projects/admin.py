1d89cfdd8a8bf5cdc7cc2ea3c49eb96621550a96
Michael Zakany <mzakany@gmail.com>
2016-09-16 12:46:12 -0400

    changes

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from models import Project
 
 class ProjectAdmin(admin.ModelAdmin): 
+	list_display = ['__unicode__','title','precedence','active','href']
+	list_editable = ['title','precedence','active','href']
+	ordering = ['precedence']
 	class Meta:
 		model = Project
 
 admin.site.register(Project,ProjectAdmin)
 
-# Register your models here.
+
